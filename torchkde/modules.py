0dbf26871cf06bc6fc750b5839560cc7ce9adc55
rudolfwilliam <kladny@gmx.net>
2025-02-08 13:41:41 +0100

    Include bandwidth checks

--- torchkde/modules.py
+++ torchkde/modules.py
@@ -25,7 +25,7 @@ KERNEL_DICT = {
 
 
 class KernelDensity(nn.Module):
-    """Analagon to the KernelDensity class in sklearn.neighbors 
+    """Analag to the KernelDensity class in sklearn.neighbors 
     (see https://github.com/scikit-learn/scikit-learn/blob/main/sklearn/neighbors/_kde.py)."""
 
     def __init__(
@@ -36,11 +36,17 @@ class KernelDensity(nn.Module):
         kernel="gaussian",
         kernel_kwargs=None
     ):
-        self.bandwidth = bandwidth
+        if not isinstance(bandwidth, str):
+            assert bandwidth > 0, "Bandwidth must be positive."
+            self.bandwidth = bandwidth**2 # square the bandwidth to match sklearn's implementation
+        else:
+            self.bandwidth = bandwidth
+        assert kernel in SUPPORTED_KERNELS, f"Kernel {kernel} not supported."
         self.kernel = kernel
         if kernel_kwargs is None:
             kernel_kwargs = {}
         self.kernel_module = KERNEL_DICT[kernel](**kernel_kwargs)
+        assert algorithm in SUPPORTED_ALGORITHMS, f"Algorithm {algorithm} not supported."
         self.algorithm = algorithm
         self.is_fitted = False
         self.n_samples = None
@@ -57,7 +63,7 @@ class KernelDensity(nn.Module):
             raise ValueError(f"Bandwidth {bandwidth} not supported")
 
 
-    def fit(self, X):
+    def fit(self, X, sample_weight=None):
         """Fit the Kernel Density model on the data.
 
         Parameters
@@ -65,6 +71,9 @@ class KernelDensity(nn.Module):
         X : torch tensor of shape (n_samples, n_features)
             List of n_features-dimensional data points.  Each row
             corresponds to a single data point.
+        sample_weight : torch tensor of shape (n_samples,), default=None
+            Weights for each sample.  If None, all samples are assigned
+            equal weight.
 
         Returns
         -------
@@ -78,7 +87,15 @@ class KernelDensity(nn.Module):
         self.tree_.build(X)
         self.bandwidth = compute_bandwidth(X, self.bandwidth)
         self.kernel_module.bandwidth = self.bandwidth
+        if sample_weight is None:
+            self.sample_weight = torch.ones(self.n_samples)
+        else:
+            assert sample_weight.shape[0] == self.n_samples, "Sample weights must have the same length as the data."
+            assert sample_weight.dim() == 1, "Sample weights must be one-dimensional."
+            assert sample_weight.min() >= 0, "Sample weights must be non-negative."
+            self.sample_weight = sample_weight
         self.is_fitted = True
+
         return self
 
     def score_samples(self, X):
@@ -104,13 +121,13 @@ class KernelDensity(nn.Module):
         # Compute normalization part from bandwidth matrix
         bw_norm = torch.sqrt(torch.det(self.bandwidth)) if check_if_mat(self.bandwidth) else self.bandwidth**(self.n_features/2)
         # looping to avoid memory issues
-        for x in X:
+        for i, x in enumerate(X):
             # Compute pairwise differences between the current point and neighbors
-            differences = x - X_neighbors
+            differences = x - X_neighbors[i]
             # Apply the kernel function to each difference
             kernel_values = self.kernel_module(differences)
             # Sum kernel values and normalize
-            density = kernel_values.sum(-1) / (bw_norm * self.n_samples)
+            density = (self.sample_weight * kernel_values).sum(-1) / (bw_norm * self.sample_weight.sum())
             # Compute the log-density
             log_density.append(density.log())
 
